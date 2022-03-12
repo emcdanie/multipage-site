6da14e17084ae10c4baf5d044fc541f997934824
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-03-12 21:19:33 +0100

    Route Paramaters
    Included route paramaters to include articles and sections when clicked.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Switch, NavLink} from  "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Contact from "./pages/Contact"
+import Article from "./pages/Article"
 
 
 
@@ -28,6 +29,9 @@ function App() {
           </Route>
           <Route  path="/contact"> 
            <Contact/>
+            </Route>
+            <Route path="/articles/:id">
+              <Article/>
             </Route>
             </Switch>
       </BrowserRouter>
