b99b643e41e58f73958144eb31de0e2a55c00c54
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-03-14 10:23:27 +0100

    QueryParameters

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,15 @@
+import { useLocation } from "react-router-dom"
+
 export default function Contact() {
+    const queryString = useLocation().search
+
+    const queryParams = new URLSearchParams(queryString)
+    const name = queryParams.get("name")
+
+    //?name=mario
+
     return ( <div >
-        <h2 > Contact Page </h2> 
+        <h2 > Hey {name}, Contact Us Here... </h2> 
         <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.Explicabo, 
             officia dolor esse perspiciatis pariatur quaerat quo nobis quam temporibus cupiditate voluptatibus, 
             omnis consequuntur obcaecati delectus quis quos hic aliquam.Maxime. </p> 
